9567c6a00193c3a9c6a1c78e4e50ec66fa8e5044
Jojo <sirjoandersonwf@gmail.com>
2022-03-29 18:13:10 -0300

    update: user basic crud

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -21,4 +21,57 @@ const store = async (req: Request, res: Response) => {
         });
 };
 
-export default { store };
+
+const list = async (req: Request, res: Response) => {
+
+    User.find()
+        .then(result => {            
+            return res.status(200).json({ message: 'Users list retrieved', data: result });
+        })
+        .catch(err => {
+            return res.status(400).json({ message: 'bad request', error: err });
+        });
+};
+
+const findById = async (req: Request, res: Response) => {
+    const { _id } = req.query;
+
+    User.findById(_id)
+        .then(result => {
+            if(!result)
+                return res.status(404).json({ message: 'User not found'});
+            return res.status(200).json({ message: 'User retrieved', data: result });
+        })
+        .catch(err => {
+            return res.status(400).json({ message: 'bad request', error: err });
+        });
+};
+
+
+const update = async (req: Request, res: Response) => {
+    
+    User.updateOne({_id: req.body._id}, req.body)
+        .then(result => {
+            if(result.matchedCount !== 0)                
+                return res.status(200).json({ message: 'User updated', data: result });
+            return res.status(404).json({ message: 'User not found'});
+        })
+        .catch(err => {
+            return res.status(400).json({ message: 'bad request', error: err });
+        });
+};
+
+const remove = async (req: Request, res: Response) => {
+    
+    User.deleteOne({_id: req.query._id})
+        .then(result => {
+            if(result.deletedCount !== 0)                
+                return res.status(200).json();
+            return res.status(404).json({ message: 'User not found'});
+        })
+        .catch(err => {
+            return res.status(400).json({ message: 'bad request', error: err });
+        });
+};
+
+export default { store, list, findById, update, remove };
--- backend/src/routes/user.route.ts
+++ backend/src/routes/user.route.ts
@@ -3,5 +3,9 @@ import UserController from '../controllers/user.controller';
 const routes = Router();
 
 routes.post('/users', UserController.store);
+routes.get('/users', UserController.list);
+routes.get('/users/findOne', UserController.findById);
+routes.put('/users', UserController.update);
+routes.delete('/users', UserController.remove);
 
 export default routes;
