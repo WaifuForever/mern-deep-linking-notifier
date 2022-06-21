2c61c400a0b5ad787c8fc2904bf1f890e67c8ecc
Waifu Forever <itachiut1r4@gmail.com>
2022-06-21 20:55:45 -0300

    update: protected routes

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -3,68 +3,63 @@ import { Request, Response, NextFunction } from 'express';
 import User from '../models/user.model';
 import jwt from '../utils/jwt.util';
 
-export const auth = () => {
-    return async (req: Request, res: Response, next: NextFunction) => {
-        if (!req.headers.authorization) {
+export const auth = async (req: Request, res: Response, next: NextFunction) => {
+    if (!req.headers.authorization) {
+        return res.status(401).json({
+            message: 'default.unauthorized',
+        });
+    }
+    const [, token] = req.headers.authorization
+        ? req.headers.authorization.split(' ')
+        : [, ''];
+
+    let payload: any = null;
+    try {
+        payload = jwt.verifyJwt(token, 1);
+    } catch (err) {
+        if (err instanceof Error)
+            return res.status(401).json({
+                message: 'default.unauthorized',
+                err: err.message,
+            });
+        else
             return res.status(401).json({
                 message: 'default.unauthorized',
             });
-        }
-        const [, token] = req.headers.authorization
-            ? req.headers.authorization.split(' ')
-            : [, ''];
+    }
 
-        let payload: any = null;
-        try {
-            payload = jwt.verifyJwt(token, 1);
-        } catch (err) {
-            if (err instanceof Error)
-                return res.status(401).json({
-                    message: 'default.unauthorized',
-                    err: err.message,
-                });
-            else
+    User.exists({
+        _id: payload._id,
+        tokenVersion: payload.tokenVersion,
+    })
+        .then(result => {
+            if (!result) {
                 return res.status(401).json({
                     message: 'default.unauthorized',
                 });
-        }
-
-        User.exists({
-            _id: payload._id,
-            tokenVersion: payload.tokenVersion,
+            }
+            req.auth = payload._id;
+            let current_time = Date.now().valueOf() / 1000;
+            if ((payload.exp - payload.iat) / 2 > payload.exp - current_time) {
+                let newToken = jwt.generateJwt(
+                    {
+                        _id: payload.id,
+                        tokenVersion: payload.tokenVersion,
+                    },
+                    1,
+                );
+                req.new_token = `${newToken}`;
+                console.log(`New Token: ${newToken}`);
+            }
+            console.log('shall pass');
+            payload = null;
+            next();
         })
-            .then(result => {
-                if (!result) {
-                    return res.status(401).json({
-                        message: 'default.unauthorized',
-                    });
-                }
-                req.auth = payload._id;
-                let current_time = Date.now().valueOf() / 1000;
-                if (
-                    (payload.exp - payload.iat) / 2 >
-                    payload.exp - current_time
-                ) {
-                    let newToken = jwt.generateJwt(
-                        {
-                            _id: payload.id,
-                            tokenVersion: payload.tokenVersion,
-                        },
-                        1,
-                    );
-                    req.new_token = `${newToken}`;
-                    console.log(`New Token: ${newToken}`);
-                }
-                console.log('shall pass');
-                payload = null;
-                next();
-            })
-            .catch(err => {
-                console.log(err);
-                return res.status(400).json({
-                    message: 'default.badRequest',
-                    err: err,
-                });
+        .catch(err => {
+            console.log(err);
+            return res.status(400).json({
+                message: 'default.badRequest',
+                err: err,
             });
-    };
+        });
 };
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,13 +1,14 @@
-import { Router, Request, Response } from 'express';
+import { Router } from 'express';
+import { auth } from '../middlewares/auth.middleware';
 import UserController from '../controllers/user.controller';
 import UserMiddleware from '../middlewares/user.middleware';
 
 const routes = Router();
 
-routes.post('/users', UserMiddleware.store, UserController.store);
+routes.post('/users', auth, UserMiddleware.store, UserController.store);
 routes.get('/users', UserMiddleware.list, UserController.list);
 routes.get('/users/findOne', UserMiddleware.findById, UserController.findById);
-routes.put('/users', UserMiddleware.update, UserController.update);
-routes.delete('/users', UserMiddleware.findById, UserController.remove);
+routes.put('/users', auth, UserMiddleware.update, UserController.update);
+routes.delete('/users', auth, UserMiddleware.findById, UserController.remove);
 
 export default routes;
